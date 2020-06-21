50620fc0fa119c9e11f19bdb290b21d6ea4e1cf1
Joel Kibona <joelvankibona@gmail.com>
2020-06-21 14:27:34 +0300

    mimetypes fix unique mime type with different extension

--- app/Http/Requests/FileExtension/FileExtensionStoreRequest.php
+++ app/Http/Requests/FileExtension/FileExtensionStoreRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests\FileExtension;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class FileExtensionStoreRequest extends FormRequest
 {
@@ -21,12 +23,26 @@ class FileExtensionStoreRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(Request $request)
     {
         return [
+            'mime_type' => ['required',
+                Rule::unique('file_extensions')->where(function ($query) use ($request) {
+                    return $query
+                        ->where('extension', $request->extension)
+                        ->where('mime_type', $request->mime_type);
+                })
+            ],
             'extension' => 'required|unique:file_extensions',
-            'mime_type' => 'required|unique:file_extensions',
             'icon' => ''
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'mime_type.unique' => 'A mime type associated with the extension exists.',
+            'extension.unique' => 'An extension associated with the mime type exists.',
         ];
     }
 }
